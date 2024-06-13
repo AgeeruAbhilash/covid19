66cc2bf50901b58b807ac18be8fbda79be47bd5b
AgeeruAbhilash <101691831+AgeeruAbhilash@users.noreply.github.com>
2024-06-13 12:59:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,8 @@ from sklearn.ensemble import RandomForestClassifier
 app = Flask(__name__)
 
 import pandas as pd
+import os
+file_path = os.path.join(os.path.dirname(__file__), 'data', 'Dataset.csv')
 data=pd.read_csv(r"https://github.com/AgeeruAbhilash/covid19/blob/main/Dataset.csv")
 data
 le=LabelEncoder()
